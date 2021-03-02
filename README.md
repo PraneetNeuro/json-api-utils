1ead79f6878589972007c547b54e845e9115c429
Praneet <praneetquantumx@gmail.com>
2021-03-02 21:43:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# Install Japi
-```shell
-npm i japi-utils
-```
-
 # Why Japi?
 1. You can see the JSON response structure
 2. You can validate the JSON response with the one that you expect
+3. You can see if the JSON response has an error property
+4. Export file with JSON response structures for a given array of APIs 
 
 # How to use Japi?
 1. responseStructure(url) returns the structure of the JSON response
 2. validate(url, structureExpected) returns if the json response received matches the one expected
+3. hasError(json) returns if the the json has an error property
+4. exportAllJSON(urls) creates a file with the structure of JSON responses for each api
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch')
 const _ = require('lodash')
+const fs = require('fs')
+const { url } = require('inspector')
 
 const responseStructure = async (url) => {
     const response = await fetch(url)
@@ -16,7 +18,49 @@ const validate = async (url, structure) => {
     return _.isEqual(structure, structureOfResponse)
 }
 
+const hasError = (json) => {
+    return Object.keys(json).some((key) => ['Error', 'error'].includes(key))
+}
+
+const exportAllJSON = async (urls, path='./jsonExport.txt') => {
+    if(typeof urls === 'string'){
+        const structure = await responseStructure(urls)
+        const data = `\n--- JSON Response for ${urls} ---\n${JSON.stringify(structure)}\n---`
+        if (fs.existsSync(path)) {
+            fs.appendFileSync(path, data)
+        } else {
+            const writeStream = fs.createWriteStream(path);
+            writeStream.write(data);
+            writeStream.end();
+        }
+    } else {
+        try {
+            if(urls.length) {
+                    const structures = await Promise.all(urls.map(async (url) => {
+                    const struct = await responseStructure(url)
+                    return `\n--- JSON Response for ${url} ---\n${JSON.stringify(struct)}\n---`
+                    }))
+                    structures.map((data) => {
+                        if (fs.existsSync(path)) {
+                            fs.appendFileSync(path, data)
+                        } else {
+                            const writeStream = fs.createWriteStream(path);
+                            writeStream.write(data);
+                            writeStream.end();
+                        }
+                    })
+            } else {
+                throw 'Expected an array of URLs or a URL'
+            }
+        } catch(err) {
+            throw err
+        }
+    }
+} 
+
 module.exports = {
     responseStructure,
-    validate
+    validate,
+    hasError, 
+    exportAllJSON
 }
